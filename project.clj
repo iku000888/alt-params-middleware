37ff2777aad178e22354f0d203ab7075606dfe88
Ikuru K <kanumaiku@gmail.com>
2016-12-17 02:42:52 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject alt-params-middleware "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Drop-in replacement of ring.middleware.params with pluggable decoder support"
+  :url "https://github.com/iku000888/alt-params-middleware/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
